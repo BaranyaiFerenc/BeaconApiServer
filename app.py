2daf02f0b450f82948c5dfe6f43a1c10032faed2
Baranyai Ferenc <baranyai.fer@gmail.com>
2025-10-22 23:03:20 +0200

    Correcting config path

--- app.py
+++ app.py
@@ -352,12 +352,14 @@ def get_camera_configuration():
     print(f"Camera configuration by: {request.user}")
     return jsonify(success=True, data=data), 200
 
+import os
+
 @app.route("/key")
 def key():
     return f"<h1>{app.config['SECRET_KEY']}</h1>"
 
 if __name__ == "__main__":
-    with open("./database/server-config.json", "r", encoding="utf-8") as f:
+    with open(os.path.join(os.getcwd(),"database/server-config.json"), "r", encoding="utf-8") as f:
         data = json.load(f)
 
     if data['server'].get('log_to_file', False):
